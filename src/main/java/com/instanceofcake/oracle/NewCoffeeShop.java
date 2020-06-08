01ca2f690b01fd21f559334f46c72401c230bb05
Satish Nayak <satishkumarnayak@gmail.com>
2020-06-08 23:17:59 +0530

    Added different solution

---
+++ src/main/java/com/instanceofcake/oracle/NewCoffeeShop.java
@@ -0,0 +1,76 @@
+package com.instanceofcake.oracle;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class NewCoffeeShop {
+
+	Map<Integer, Integer> billToCountMap = new HashMap<Integer, Integer>();
+
+	public boolean solution(int[] input) {
+		boolean result = true;
+
+		billToCountMap.put(5, 0);
+		billToCountMap.put(10, 0);
+		billToCountMap.put(20, 0);
+
+		for (int i = 0; i < input.length; i++) {
+			int fiveBillCount = 0;
+			int tenBillCount = 0;
+
+			if (i == 0) {
+				if (input[0] > 5) {
+					result = false;
+					return result;
+				}
+			}
+			int changeReturn = input[i] - 5;
+
+			if (changeReturn == 0) {
+				fiveBillCount = billToCountMap.get(5);
+				fiveBillCount++;
+				billToCountMap.put(5, fiveBillCount);
+			}
+			if (changeReturn == 5) {
+				fiveBillCount = billToCountMap.get(5);
+				if (fiveBillCount <= 0) {
+					result = false;
+					return result;
+				}
+				fiveBillCount--;
+				billToCountMap.put(5, fiveBillCount);
+				int inputCount = billToCountMap.get(input[i]);
+				inputCount++;
+				billToCountMap.put(input[i], inputCount);
+			}
+			if (changeReturn == 15) {
+				tenBillCount = billToCountMap.get(10);
+				fiveBillCount = billToCountMap.get(5);
+				if (fiveBillCount == 0 || (tenBillCount <= 0 && fiveBillCount < 3)) {
+					result = false;
+					return result;
+				}
+				if (tenBillCount > 0 && fiveBillCount >= 2) {
+					tenBillCount--;
+					billToCountMap.put(10, tenBillCount);
+					fiveBillCount--;
+					billToCountMap.put(5, fiveBillCount);
+					int inputCount = billToCountMap.get(input[i]);
+					inputCount++;
+					billToCountMap.put(input[i], inputCount);
+				}
+				if (fiveBillCount >= 3) {
+					fiveBillCount = fiveBillCount - 3;
+					billToCountMap.put(5, fiveBillCount);
+					int inputCount = billToCountMap.get(input[i]);
+					inputCount++;
+					billToCountMap.put(input[i], inputCount);
+				}
+
+			}
+		}
+
+		return result;
+	}
+
+}
---
+++ src/test/java/com/instanceofcake/oracle/NewCoffeeShopTest.java
@@ -0,0 +1,129 @@
+package com.instanceofcake.oracle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NewCoffeeShopTest {
+
+	@Test
+	public void testSolutionExampleTwoWhenTen() {
+		NewCoffeeShop coffeeShop = new NewCoffeeShop();
+		int[] input = { 10 };
+		boolean result = coffeeShop.solution(input);
+		assertFalse(result);
+	}
+
+	@Test
+	public void testSolutionWhenInputFive() {
+		NewCoffeeShop coffeeShop = new NewCoffeeShop();
+		int[] input = { 5 };
+		boolean result = coffeeShop.solution(input);
+		assertTrue(result);
+	}
+
+	@Test
+	public void testSolutionWhenInputFiveFiveFive() {
+		NewCoffeeShop coffeeShop = new NewCoffeeShop();
+		int[] input = { 5, 5, 5 };
+		boolean result = coffeeShop.solution(input);
+		assertTrue(result);
+	}
+
+	@Test
+	public void testSolutionWhenInputFiveTen() {
+		NewCoffeeShop coffeeShop = new NewCoffeeShop();
+		int[] input = { 5, 10 };
+		boolean result = coffeeShop.solution(input);
+		assertTrue(result);
+	}
+
+	@Test
+	public void testSolutionWhenInputFiveTenTen() {
+		NewCoffeeShop coffeeShop = new NewCoffeeShop();
+		int[] input = { 5, 10, 10 };
+		boolean result = coffeeShop.solution(input);
+		assertFalse(result);
+	}
+
+	@Test
+	public void testSolutionWhenInputTripleFiveTwenty() {
+		NewCoffeeShop coffeeShop = new NewCoffeeShop();
+		int[] input = { 5, 5, 5, 20 };
+		boolean result = coffeeShop.solution(input);
+		assertTrue(result);
+	}
+
+	@Test
+	public void testSolutionExampleOneWhenInputTripleFiveAndTenAndTwenty() {
+		NewCoffeeShop coffeeShop = new NewCoffeeShop();
+		int[] input = { 5, 5, 5, 10, 20 };
+		boolean result = coffeeShop.solution(input);
+		assertTrue(result);
+	}
+
+	@Test
+	public void testSolutionExampleTwoWhenInputFiveAndFiveAndTen() {
+		NewCoffeeShop coffeeShop = new NewCoffeeShop();
+		int[] input = { 5, 5, 10 };
+		boolean result = coffeeShop.solution(input);
+		assertTrue(result);
+	}
+
+	@Test
+	public void testSolutionExampleThreeWhenInputTenAndTen() {
+		NewCoffeeShop coffeeShop = new NewCoffeeShop();
+		int[] input = { 10, 10 };
+		boolean result = coffeeShop.solution(input);
+		assertFalse(result);
+	}
+
+	@Test
+	public void testSolutionExampleFourWhenInputDoubleFiveDoubleTenAndTwenty() {
+		NewCoffeeShop coffeeShop = new NewCoffeeShop();
+		int[] input = { 5, 5, 10, 10, 20 };
+		boolean result = coffeeShop.solution(input);
+		assertFalse(result);
+	}
+
+	@Test
+	public void testSolutionFirstOrderGreaterThanFive() {
+		NewCoffeeShop coffeeShop = new NewCoffeeShop();
+		int[] input = { 10, 5, 10 };
+		boolean result = coffeeShop.solution(input);
+		assertFalse(result);
+	}
+
+	@Test
+	public void testSolutionWhenInputFiveAndFive() {
+		NewCoffeeShop coffeeShop = new NewCoffeeShop();
+		int[] input = { 5, 5 };
+		boolean result = coffeeShop.solution(input);
+		assertTrue(result);
+	}
+
+	@Test
+	public void testSolutionDemo() {
+		NewCoffeeShop coffeeShop = new NewCoffeeShop();
+		int[] input = { 5, 5, 5, 10, 5, 5, 10, 20, 20, 20 };
+		boolean result = coffeeShop.solution(input);
+		assertFalse(result);
+	}
+
+	@Test
+	public void testSolutionDemo2() {
+		NewCoffeeShop coffeeShop = new NewCoffeeShop();
+		int[] input = { 5, 5, 5, 10, 5, 20, 5, 10, 5, 20 };
+		boolean result = coffeeShop.solution(input);
+		assertTrue(result);
+	}
+	
+	@Test
+	public void testSolutionDemo3() {
+		NewCoffeeShop coffeeShop = new NewCoffeeShop();
+		int[] input = { 5, 5, 5, 10, 5, 20, 5, 10, 5, 20 };
+		boolean result = coffeeShop.solution(input);
+		assertTrue(result);
+	}
+
+}
